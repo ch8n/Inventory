485e07076af37c981f391865695e190c7b8728f6
Ashutosh Gupta <ashusanugupta999@gmail.com>
2023-10-25 15:52:32 +0530

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "ch8n.dev.inventory"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "ch8n.dev.inventory"
@@ -53,9 +53,9 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
