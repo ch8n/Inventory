d46efd8e4e1ed5ef1ea0bcd5f7b6c7bdb482bb0a
ch8n <chetan.garg36@gmail.com>
2023-11-13 14:07:34 +0530

    added order create

--- app/src/main/java/ch8n/dev/inventory/ui/screens/CreateOrderScreen.kt
+++ app/src/main/java/ch8n/dev/inventory/ui/screens/CreateOrderScreen.kt
@@ -389,6 +389,7 @@ fun CreateOrderContent(
                                     createdAt = System.currentTimeMillis()
                                 )
                             )
+                            context.toast("Creating order!")
                             navigator.back()
                         },
                         modifier = Modifier.fillMaxWidth()
